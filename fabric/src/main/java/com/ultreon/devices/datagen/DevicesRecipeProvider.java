5e2e10c123577b6e9dcc9ca605107a688b0c9fe1
Qboi123 <qboiwastaken@gmail.com>
2023-06-21 20:58:13 +0200

    Recipe Fix?

--- fabric/src/main/java/com/ultreon/devices/datagen/DevicesRecipeProvider.java
+++ fabric/src/main/java/com/ultreon/devices/datagen/DevicesRecipeProvider.java
@@ -1,8 +1,13 @@
 package com.ultreon.devices.datagen;
 
+import com.ultreon.devices.Devices;
+import com.ultreon.devices.block.PrinterBlock;
+import com.ultreon.devices.block.RouterBlock;
 import com.ultreon.devices.init.DeviceBlocks;
 import com.ultreon.devices.init.DeviceItems;
 import com.ultreon.devices.init.DeviceTags;
+import com.ultreon.devices.init.ModTags;
+import com.ultreon.devices.item.FlashDriveItem;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.recipes.FinishedRecipe;
@@ -10,6 +15,7 @@ import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.DyeItem;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
 
@@ -24,35 +30,100 @@ public class DevicesRecipeProvider extends FabricRecipeProvider {
     public void buildRecipes(Consumer<FinishedRecipe> exporter) {
         DeviceBlocks.LAPTOPS.getMap().forEach(((dyeColor, blockRegistrySupplier) -> laptop(exporter, blockRegistrySupplier.get(), dyeColor)));
 
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.DECORATIONS, DeviceItems.COMPONENT_FULL_MOTHERBOARD.get())
-                .requires(DeviceItems.COMPONENT_CPU.get())
-                .requires(DeviceItems.COMPONENT_GPU.get())
-                .requires(DeviceItems.COMPONENT_RAM.get())
-                .requires(DeviceItems.COMPONENT_WIFI.get())
-                .requires(DeviceItems.COMPONENT_MOTHERBOARD.get())
-                .unlockedBy(getHasName(DeviceItems.COMPONENT_MOTHERBOARD.get()), has(DeviceItems.COMPONENT_MOTHERBOARD.get()))
-                .unlockedBy(getHasName(DeviceItems.COMPONENT_CPU.get()), has(DeviceItems.COMPONENT_CPU.get()))
-                .unlockedBy(getHasName(DeviceItems.COMPONENT_GPU.get()), has(DeviceItems.COMPONENT_GPU.get()))
-                .unlockedBy(getHasName(DeviceItems.COMPONENT_RAM.get()), has(DeviceItems.COMPONENT_RAM.get()))
-                .unlockedBy(getHasName(DeviceItems.COMPONENT_WIFI.get()), has(DeviceItems.COMPONENT_WIFI.get()))
+        //***********************//
+        //      Flash Drives     //
+        //***********************//
+        for (FlashDriveItem flashDrive : DeviceItems.getAllFlashDrives()) {
+            DyeColor color = flashDrive.getColor();
+            new ShapedRecipeBuilder(RecipeCategory.TOOLS, flashDrive, 1)
+                    .pattern("did")
+                    .pattern("pfp")
+                    .pattern("pcp")
+                    .define('d', DyeItem.byColor(color))
+                    .define('i', Items.IRON_INGOT)
+                    .define('p', DeviceItems.PLASTIC_FRAME.get())
+                    .define('f', DeviceItems.COMPONENT_FLASH_CHIP.get())
+                    .define('c', DeviceItems.COMPONENT_CIRCUIT_BOARD.get())
+                    .unlockedBy("has_flash_chip", has(DeviceItems.COMPONENT_FLASH_CHIP.get()))
+                    .group(Devices.MOD_ID + ":laptop")
+                    .save(exporter);
+        }
+
+        //******************//
+        //     Printers     //
+        //******************//
+        for (PrinterBlock printer : DeviceBlocks.getAllPrinters()) {
+            new ShapedRecipeBuilder(RecipeCategory.TOOLS, printer, 1)
+                    .pattern("psp")
+                    .pattern("mcb")
+                    .pattern("pdp")
+                    .define('d', DyeItem.byColor(printer.getColor()))
+                    .define('p', DeviceItems.PLASTIC_FRAME.get())
+                    .define('s', DeviceItems.COMPONENT_SCREEN.get())
+                    .define('m', DeviceItems.COMPONENT_SMALL_ELECTRIC_MOTOR.get())
+                    .define('c', DeviceItems.COMPONENT_CARRIAGE.get())
+                    .define('b', DeviceItems.COMPONENT_CONTROLLER_UNIT.get())
+                    .unlockedBy("has_carriage", has(DeviceItems.COMPONENT_CARRIAGE.get()))
+                    .group(Devices.MOD_ID + ":printer")
+                    .save(exporter);
+        }
+
+        //*****************//
+        //     Routers     //
+        //*****************//
+        for (RouterBlock router : DeviceBlocks.getAllRouters()) {
+            new ShapedRecipeBuilder(RecipeCategory.TOOLS, router, 1)
+                    .pattern("rdr")
+                    .pattern("ppp")
+                    .pattern("wcb")
+                    .define('d', DyeItem.byColor(router.getColor()))
+                    .define('r', Items.END_ROD)
+                    .define('p', DeviceItems.PLASTIC_FRAME.get())
+                    .define('w', DeviceItems.COMPONENT_WIFI.get())
+                    .define('c', DeviceItems.COMPONENT_CIRCUIT_BOARD.get())
+                    .define('b', DeviceItems.COMPONENT_BATTERY.get())
+                    .unlockedBy("has_circuit_board", has(DeviceItems.COMPONENT_CIRCUIT_BOARD.get()))
+                    .group(Devices.MOD_ID + ":router")
+                    .save(exporter);
+        }
+
+        //*************************//
+        //     Component Items     //
+        //*************************//
+        new ShapedRecipeBuilder(RecipeCategory.MISC, DeviceItems.COMPONENT_FLASH_CHIP.get(), 1)
+                .pattern("iri")
+                .pattern("ppp")
+                .pattern("ggg")
+                .define('p', DeviceItems.PLASTIC_FRAME.get())
+                .define('i', Items.IRON_INGOT)
+                .define('r', Items.REDSTONE)
+                .define('g', Items.GOLD_NUGGET)
+                .unlockedBy("has_laptop", has(ModTags.Items.LAPTOPS))
+                .save(exporter);
+        new ShapelessRecipeBuilder(RecipeCategory.MISC, DeviceItems.COMPONENT_MOTHERBOARD_FULL.get(), 1)
+                .requires(DeviceItems.COMPONENT_CPU.get(), 1)
+                .requires(DeviceItems.COMPONENT_GPU.get(), 1)
+                .requires(DeviceItems.COMPONENT_RAM.get(), 1)
+                .requires(DeviceItems.COMPONENT_WIFI.get(), 1)
+                .requires(DeviceItems.COMPONENT_MOTHERBOARD.get(), 1)
+                .unlockedBy("has_motherboard", has(DeviceItems.COMPONENT_MOTHERBOARD.get()))
                 .save(exporter);
     }
 
     public static void laptop(Consumer<FinishedRecipe> exporter, ItemLike laptop, DyeColor color) {
         ShapedRecipeBuilder.shaped(RecipeCategory.DECORATIONS, laptop)
                 .define('+', DyeUtils.getWoolFromDye(color))
-                .define('/', Items.NETHERITE_INGOT)
+                .define('/', Items.IRON_INGOT)
                 .define('#', DeviceItems.COMPONENT_SCREEN.get())
-                .define('|', Items.QUARTZ)
+                .define('.', Items.IRON_NUGGET)
                 .define('$', DeviceItems.COMPONENT_BATTERY.get())
+                .define('@', DeviceItems.COMPONENT_MOTHERBOARD_FULL.get())
                 .define('O', DeviceTags.Items.INTERNAL_STORAGE)
                 .pattern("+#+")
-                .pattern("/$/")
-                .pattern("|O|").group("devices:laptop")
+                .pattern(".@$")
+                .pattern("/O/").group("devices:laptop")
                 .unlockedBy(getHasName(Items.NETHERITE_INGOT), has(Items.NETHERITE_INGOT))
-                .unlockedBy(getHasName(DeviceItems.COMPONENT_FULL_MOTHERBOARD.get()), has(DeviceItems.COMPONENT_FULL_MOTHERBOARD.get()))
-                .unlockedBy(getHasName(DeviceItems.COMPONENT_BATTERY.get()), has(DeviceItems.COMPONENT_BATTERY.get()))
-                .unlockedBy(getHasName(DeviceItems.COMPONENT_HARD_DRIVE.get()), has(DeviceTags.Items.INTERNAL_STORAGE))
+                .unlockedBy(getHasName(DeviceItems.COMPONENT_MOTHERBOARD_FULL.get()), has(DeviceItems.COMPONENT_MOTHERBOARD_FULL.get()))
                 .save(exporter);
     }
 }
